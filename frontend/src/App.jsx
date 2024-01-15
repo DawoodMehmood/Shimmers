08e475d085238fe62a94ea58f572882071947c4f
Dawood Mehmood <dawoodmehmood52222@gmail.com>
2024-01-15 14:51:43 +0500

    tested out gauge component library

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GaugeComponent from "react-gauge-component";
+
+function App() {
+  return (
+    <>
+      <GaugeComponent value={10} />
+    </>
+  );
+}
+
+export default App;
